1a8822aa05b058e200c58387966ceec11f8c6747
PC-EJOUANJAN\ejouanjan <eliott6@live.fr>
2022-12-20 12:24:45 +0100

    make the start screen

--- app/src/main/java/com/eliott/training/sevenminutesworkout/MainActivity.kt
+++ app/src/main/java/com/eliott/training/sevenminutesworkout/MainActivity.kt
@@ -2,10 +2,19 @@ package com.eliott.training.sevenminutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+            Log.d("TAG", "onCreate: toto")
+        }
     }
 }
